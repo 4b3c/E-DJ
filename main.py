ddc98fd41e30dff4db0204dfe860ee3878b6b6be
Abram <58718191+4b3c@users.noreply.github.com>
2023-05-20 21:46:55 -0700

    it makes square waves too now

--- main.py
+++ main.py
@@ -2,15 +2,14 @@ import numpy as np
 import math
 import sounddevice as sd
 
-frequency = 440
+frequency = 200
 sample_rate = 44100
-duration = 5
-volume = 0.05
+duration = 0.7
+volume = 0.003
 
 t = np.arange(int(sample_rate * duration))
-raw_data = np.sin(2 * np.pi * frequency * (t**(1/1.2)) / sample_rate) * volume
-
-print(raw_data[:10])
+raw_data = np.sin(2 * np.pi * frequency * t / sample_rate) * volume
+raw_data += np.where(raw_data > 0, 1, -1)
 
 sd.play(raw_data, sample_rate)
 sd.wait()
