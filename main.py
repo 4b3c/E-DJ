3662f0b8e83d1423971e23c97877c83e2ede9427
Abram <58718191+4b3c@users.noreply.github.com>
2023-05-20 21:09:09 -0700

    made it play different notes and fade them

--- main.py
+++ main.py
@@ -2,13 +2,24 @@ import numpy as np
 import math
 import sounddevice as sd
 
-frequency = 20
-sample_rate = 44000
-duration = 1
-volume = 0.05
+notes = [261.63, 293.66, 329.63, 349.23, 392.00, 440.00, 493.88, 523.25]
+durations = [0.3, 0.3, 0.3, 0.3, 0.3, 0.3, 0.3, 0.7]
 
-t = np.arange(int(sample_rate * duration))
-raw_data = np.sin(2 * np.pi * frequency * t / sample_rate) * volume
+sample_rate = 44100
+volume = 0.1
+fade_duration = 0.01  # Duration of crossfade between notes
+
+fade_samples = int(sample_rate * fade_duration)
+
+t = np.arange(int(sample_rate * durations[0]))
+raw_data = np.sin(2 * np.pi * notes[0] * t / sample_rate) * volume
+
+for frequency, duration in zip(notes[1:], durations[1:]):
+	t = np.arange(int(sample_rate * duration))
+	new_note = np.sin(2 * np.pi * frequency * t / sample_rate) * volume
+	raw_data[-fade_samples:] *= np.linspace(1, 0, fade_samples)  # Apply fade-out to the end of the previous note
+	new_note[:fade_samples] *= np.linspace(0, 1, fade_samples)  # Apply fade-in to the beginning of the new note
+	raw_data = np.concatenate((raw_data, new_note))
 
 print(raw_data[:10])
 
