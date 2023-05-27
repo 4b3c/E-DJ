971b751613307ea50485d129c32849a18e860006
Abram <58718191+4b3c@users.noreply.github.com>
2023-05-27 10:16:02 -0700

    idk tbh

--- main.py
+++ main.py
@@ -9,8 +9,15 @@ volume = 0.003
 
 t = np.arange(int(sample_rate * duration))
 
+print(t.shape)
+
 raw_data = np.sin(2 * np.pi * frequency * t / sample_rate) * volume
-raw_data += np.where(raw_data > 0, 1, -1)
+bop = (np.sin((2 * np.pi * frequency * (t[0:20000] - 5000)) / sample_rate) * (10000 / ((t[0:20000] - 5000)**0.56))) / 17.915
+print(bop.shape)
+
+raw_data[20000:20000 + bop.shape[0]] += bop
+print(raw_data[25000])
 
 sd.play(raw_data, sample_rate)
 sd.wait()
+
--- square.py
+++ square.py
@@ -10,7 +10,7 @@ volume = 0.003
 t = np.arange(int(sample_rate * duration))
 
 raw_data = np.sin(2 * np.pi * frequency * t / sample_rate) * volume
-raw_data += np.where(raw_data > 0, 1, -1)
+raw_data += np.where(raw_data > 0, volume, -volume)
 
 sd.play(raw_data, sample_rate)
 sd.wait()
